a903bb9b0d37a5d521c10e1b5a76175a5e7a7877
Tanuja Mungase <tanujamungase0@gmail.com>
2026-08-13 14:06:30 +0530

    Change app run configuration for production

--- app.py
+++ app.py
@@ -771,11 +771,10 @@ def live():
 # =========================================================
 
 if __name__ == "__main__":
-
     init_database()
 
     app.run(
-        debug=True,
-        host="127.0.0.1",
-        port=5000
+        debug=False,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
